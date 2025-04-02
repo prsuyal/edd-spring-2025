96839b4757c21283aa234f4e673ec36908906b07
Pranshu Suyal <suyalpranshu@gmail.com>
2025-04-02 12:25:39 -0400

    set up repo

---
+++ mobile/mobile/mobile/mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileApp.swift
+//  mobile
+//
+//  Created by Pranshu Suyal on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
